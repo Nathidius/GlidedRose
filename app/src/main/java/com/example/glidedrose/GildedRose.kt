787009b3c21643225394ed3196011d25fd986293
Jarosław Kamykowski <jaroslaw.kamykowski@jar-apps.com>
2023-05-14 14:47:55 +0200

    solution

--- app/src/main/java/com/example/glidedrose/GildedRose.kt
+++ app/src/main/java/com/example/glidedrose/GildedRose.kt
@@ -3,56 +3,47 @@ package com.example.glidedrose
 class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+        for (item in items) {
+            // Decrease sellIn for all items except Sulfuras
+            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                item.sellIn -= 1
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+            // Update quality for all items except Sulfuras
+            when {
+                item.name == "Aged Brie" -> updateAgedBrie(item)
+                item.name == "Sulfuras, Hand of Ragnaros" -> continue
+                item.name.startsWith("Backstage pass") -> updateBackstagePass(item)
+                item.name.startsWith("Conjured") -> updateConjuredItem(item)
+                else -> updateNormalItem(item)
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+            // Ensure quality bounds
+            item.quality = item.quality.coerceIn(0, 50)
         }
     }
 
-}
+    private fun updateNormalItem(item: Item) {
+        val qualityChange = if (item.sellIn <= 0) -2 else -1
+        item.quality += qualityChange
+    }
 
+    private fun updateAgedBrie(item: Item) {
+        val qualityChange = if (item.sellIn <= 0) 2 else 1
+        item.quality += qualityChange
+    }
+
+    private fun updateBackstagePass(item: Item) {
+        when {
+            item.sellIn <= 0 -> item.quality = 0
+            item.sellIn <= 5 -> item.quality += 3
+            item.sellIn <= 10 -> item.quality += 2
+            else -> item.quality += 1
+        }
+    }
+
+    private fun updateConjuredItem(item: Item) {
+        val qualityChange = if (item.sellIn <= 0) -4 else -2
+        item.quality += qualityChange
+    }
+}
--- app/src/test/java/com/example/glidedrose/GildedRoseTest.kt
+++ app/src/test/java/com/example/glidedrose/GildedRoseTest.kt
@@ -6,11 +6,117 @@ import org.junit.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
-        val items = listOf(Item("foo", 0, 0))
+    fun testUpdateQuality_NormalItem() {
+        val items = listOf(
+            Item("Normal Item", 5, 10),
+            Item("Normal Item Outdated", 0, 10),
+            Item("Normal Item Outdated", -5, 10)
+        )
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+
+        // Assert the updated sellIn for each item
+        assertEquals(4, items[0].sellIn)
+        assertEquals(-1, items[1].sellIn)
+        assertEquals(-6, items[2].sellIn)
+
+        // Assert the updated quality for each item
+        assertEquals(9, items[0].quality)
+        assertEquals(8, items[1].quality)
+        assertEquals(8, items[2].quality)
+    }
+
+    @Test
+    fun testUpdateQuality_AgedBrie() {
+        val items = listOf(
+            Item("Aged Brie", 2, 10),
+            Item("Aged Brie", 0, 10),
+            Item("Aged Brie", -2, 10)
+        )
+        val app = GildedRose(items)
+        app.updateQuality()
+
+        // Assert the updated sellIn for each item
+        assertEquals(1, items[0].sellIn)
+        assertEquals(-1, items[1].sellIn)
+        assertEquals(-3, items[2].sellIn)
+
+        // Assert the updated quality for each item
+        assertEquals(11, items[0].quality)
+        assertEquals(12, items[1].quality)
+        assertEquals(12, items[2].quality)
+    }
+
+    @Test
+    fun testUpdateQuality_Sulfuras() {
+        val items = listOf(
+            Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            Item("Sulfuras, Hand of Ragnaros", -2, 80),
+        )
+        val app = GildedRose(items)
+        app.updateQuality()
+
+        // Assert the updated sellIn for each item
+        assertEquals(0, items[0].sellIn)
+        assertEquals(-2, items[1].sellIn)
+
+        // Assert the updated quality for each item
+        assertEquals(80, items[0].quality)
+        assertEquals(80, items[1].quality)
+    }
+
+    @Test
+    fun testUpdateQuality_BackstagePass() {
+        val items = listOf(
+            Item("Backstage pass", 15, 10),
+            Item("Backstage pass", 10, 10),
+            Item("Backstage pass", 8, 10),
+            Item("Backstage pass", 5, 10),
+            Item("Backstage pass", 3, 10),
+            Item("Backstage pass", 0, 10),
+            Item("Backstage pass", -5, 10)
+        )
+        val app = GildedRose(items)
+        app.updateQuality()
+
+        // Assert the updated sellIn for each item
+        assertEquals(14, items[0].sellIn)
+        assertEquals(9, items[1].sellIn)
+        assertEquals(7, items[2].sellIn)
+        assertEquals(4, items[3].sellIn)
+        assertEquals(2, items[4].sellIn)
+        assertEquals(-1, items[5].sellIn)
+        assertEquals(-6, items[6].sellIn)
+
+        // Assert the updated quality for each item
+        assertEquals(11, items[0].quality)
+        assertEquals(12, items[1].quality)
+        assertEquals(12, items[2].quality)
+        assertEquals(13, items[3].quality)
+        assertEquals(13, items[4].quality)
+        assertEquals(0, items[5].quality)
+        assertEquals(0, items[6].quality)
+    }
+
+    @Test
+    fun testUpdateQuality_ConjuredItem() {
+        val items = listOf(
+            Item("Conjured Item", 3, 10),
+            Item("Conjured Item", 0, 10),
+            Item("Conjured Item", -2, 10)
+        )
+        val app = GildedRose(items)
+        app.updateQuality()
+
+        // Assert the updated sellIn for each item
+        assertEquals(2, items[0].sellIn)
+        assertEquals(-1, items[1].sellIn)
+        assertEquals(-3, items[2].sellIn)
+
+        // Assert the updated quality for each item
+        assertEquals(8, items[0].quality)
+        assertEquals(6, items[1].quality)
+        assertEquals(6, items[2].quality)
     }
 }
 
